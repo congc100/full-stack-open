302c78a8984f5d3a27a48dfe7667c055d5fa8036
congc100 <2743196260@qq.com>
2024-08-15 03:14:35 +0300

    2.16 - 2.17 finished

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react'
 import personService from './services/persons'
+import Notification from './components/Notification'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
+
+  const [message, setMessage] = useState(null)
+  const [type, setType] = useState('message')
 
   const [filter, setFilter] = useState('')
   const [newName, setNewName] = useState('')
@@ -16,6 +20,20 @@ const App = () => {
       .getAll()
       .then(response => setPersons(response.data))
   }, [])
+
+  const notify = m => {
+    console.log('notify', m)
+    setType('message')
+    setMessage(m)
+    setTimeout(() => setMessage(null), 2000)
+  }
+
+  const errorify = m => {
+    console.log('errorify', m)
+    setType('error')
+    setMessage(m)
+    setTimeout(() => setMessage(null), 2000)
+  }
 
   const onSubmit = e => {
     e.preventDefault()
@@ -29,15 +47,21 @@ const App = () => {
         .update(current.id, { ...current, number: newNumber })
         .then(response => {
           console.log('update res', response)
+          notify(`${response.data.name} updated`)
           setPersons(persons.map(p => p.id === response.data.id ? response.data : p))
           setNewName('')
           setNewNumber('')
+        })
+        .catch(error => {
+          console.log('update error', error)
+          errorify(`Information of ${current.name} has already been removed from server`)
         })
     } else {
       personService
         .create({ name: newName, number: newNumber })
         .then(response => {
           console.log('submit res', response)
+          notify(`${response.data.name} added`)
           setPersons([...persons, response.data])
           setNewName('')
           setNewNumber('')
@@ -50,6 +74,7 @@ const App = () => {
       .remove(id)
       .then(response => {
         console.log('remove res', response)
+        notify(`${response.data.name} deleted`)
         setPersons(persons.filter(p => p.id !== id))
       })
   }
@@ -57,6 +82,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification type={type} message={message} />
       <Filter filter={filter} setFilter={setFilter} />
       <h3>Add a new</h3>
       <PersonForm
